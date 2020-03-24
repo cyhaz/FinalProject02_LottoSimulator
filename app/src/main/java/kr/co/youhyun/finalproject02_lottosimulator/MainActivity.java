d6a89fa7814b54652cb1dc35189fc5367018524f
cyhaz <cyhaz@naver.com>
2020-03-24 11:15:07 +0900

    2등 3등 로직

--- app/src/main/java/kr/co/youhyun/finalproject02_lottosimulator/MainActivity.java
+++ app/src/main/java/kr/co/youhyun/finalproject02_lottosimulator/MainActivity.java
@@ -146,6 +146,20 @@ public class MainActivity extends BaseActivity {
 
         } else if (correctCount == 5) {
             // 2등? 3등? 재검사 => 보너스번호 맞췄나?
+            // 내 번호중에 보너스번호와 같은게 있나?
+            boolean hasBonusNum = false;
+            for (int myNum : myLottoNumArr) {
+                if (myNum == bonusNum) {
+                    hasBonusNum = true;
+                    break;
+                }
+            }
+            if (hasBonusNum) {
+                winMoneyAmount += 75000000;
+            } else {
+                winMoneyAmount += 1500000;
+            }
+
         } else if (correctCount == 4) {
             // 4등
             winMoneyAmount += 50000;
