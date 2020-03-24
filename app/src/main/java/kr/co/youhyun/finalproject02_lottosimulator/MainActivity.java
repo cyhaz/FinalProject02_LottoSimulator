603fc48c459e41a5f168f4ca7b00fc597cd87314
cyhaz <cyhaz@naver.com>
2020-03-24 10:18:00 +0900

    로또 당첨번호 생성  - Log로만 확인  - 랜덤넘버 생성 / 중복검사

--- app/src/main/java/kr/co/youhyun/finalproject02_lottosimulator/MainActivity.java
+++ app/src/main/java/kr/co/youhyun/finalproject02_lottosimulator/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import kr.co.youhyun.finalproject02_lottosimulator.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
 
+    int[] winLottoNumArr = new int[6];
+    int bonusNum = 0;
     ActivityMainBinding binding = null;
 
     @Override
@@ -22,10 +26,54 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.buyOneLottoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                당첨번호 생성 => 텍스트뷰에 반영
+                makeWinLottoNum();
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
 
     }
+
+    void makeWinLottoNum() {
+//        6개의 숫자(배열) + 보너스번호 1개(int변수)
+//        => 이 함수에서만이 아니라, 다른곳에서도 쓸 예정 (당첨등수 확인 때도 사용)
+//        => 배열과 변수는 멤버변수로 생성!!
+
+//        당첨번호 + 보너스번호를 모두 0으로 초기화
+//        (이미 뽑은 번호가 있다면 모두 날리자! -> 초기화)
+        for (int i=0; i<winLottoNumArr.length; i++) {
+            winLottoNumArr[i] = 0;
+        }
+        bonusNum = 0;
+
+//        로또번호 6개 생성
+//        1) 1~45 사이의 숫자
+//        2) 중복 허용 X
+        for (int i=0; i<winLottoNumArr.length; i++) {
+//            1~45의 숫자 뽑고, 중복이 아니면 당첨번호로 선정. (중복이 아닐때까지 계속 뽑쟈)
+            while (true) {
+                int randomNum = (int) (Math.random()*45+1);
+                boolean isDuplOk = true;
+                for (int winNum : winLottoNumArr) {
+                    if (winNum == randomNum) {
+                        isDuplOk = false;
+                        break;
+                    }
+                }
+                if (isDuplOk) {
+                    winLottoNumArr[i] = randomNum;
+                    Log.i("당첨번호", randomNum+"");
+                    break;
+                }
+            }
+        }
+    }
+
 }
