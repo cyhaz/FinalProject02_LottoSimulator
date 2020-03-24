b0ad2bf94f1f133e4b07a783e7efd8df613bfe8e
cyhaz <cyhaz@naver.com>
2020-03-24 10:36:15 +0900

    당첨번호 정렬 + TextView 출력

--- app/src/main/java/kr/co/youhyun/finalproject02_lottosimulator/MainActivity.java
+++ app/src/main/java/kr/co/youhyun/finalproject02_lottosimulator/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import kr.co.youhyun.finalproject02_lottosimulator.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
 
+    List<TextView> winNumTxtList = new ArrayList<>();
     int[] winLottoNumArr = new int[6];
     int bonusNum = 0;
     ActivityMainBinding binding = null;
@@ -38,6 +44,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        winNumTxtList.add((binding.winLottoNumTxt01));
+        winNumTxtList.add((binding.winLottoNumTxt02));
+        winNumTxtList.add((binding.winLottoNumTxt03));
+        winNumTxtList.add((binding.winLottoNumTxt04));
+        winNumTxtList.add((binding.winLottoNumTxt05));
+        winNumTxtList.add((binding.winLottoNumTxt06));
 
     }
 
@@ -74,6 +87,13 @@ public class MainActivity extends BaseActivity {
                 }
             }
         }
+//        6개의 당첨번호를 작은 숫자부터 정렬
+        Arrays.sort(winLottoNumArr);
+        for (int i=0; i<winLottoNumArr.length; i++) {
+            winNumTxtList.get(i).setText(winLottoNumArr[i]+"");   // +"" 붙이면 강제로 문자로 변경
+
+        }
+
     }
 
 }
